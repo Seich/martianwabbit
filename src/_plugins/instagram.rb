e0f5372172bdfb2285dca01a38f1c5f701a8cb3b
David Diaz <seich@martianwabbit.com>
2020-10-12 19:09:35 -0600

    Adding instagram login.

--- src/_plugins/instagram.rb
+++ src/_plugins/instagram.rb
@@ -24,6 +24,17 @@ module Jekyll
 
         driver.get("https://instagram.com/seichleon")
 
+        begin 
+          username = driver.find_element(css: 'input[name="username"]')
+          username.send_keys "seichleon"
+
+          password = driver.find_element(css: 'input[name="password"]')
+          password.send_keys ENV["INSTAGRAM_PASSWORD"]
+          password.submit
+        rescue
+          puts "Login not necessary."
+        end
+
         links = driver.find_elements(css: 'a[href^="/p/"]')
         photos = links.first(@amount).map do |link|
           img = link.find_element(css: "img")
